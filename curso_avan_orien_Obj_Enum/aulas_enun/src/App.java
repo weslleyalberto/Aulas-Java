60b5d188b7c3759fcaccf3c66d260a12a46a757e
Weslley P. Alberto <weslleyalberto@gmail.com>
2021-12-16 11:27:10 -0300

    Adicioonado função para testar subreposição (@Override)

--- curso_avan_orien_Obj_Enum/aulas_enun/src/App.java
+++ curso_avan_orien_Obj_Enum/aulas_enun/src/App.java
@@ -1,5 +1,14 @@
+import java.util.Date;
+import entities.Pedido;
+import entities.enums.OrderStatus;
+
 public class App {
     public static void main(String[] args) throws Exception {
-       
+       Pedido pedido = new Pedido(1080,new Date(), OrderStatus.PAGAMENTO_PENDENTE);
+       OrderStatus os1 = OrderStatus.ENTREGUE;
+       OrderStatus os2 = OrderStatus.valueOf("PROCESSANDO");
+       System.out.println(pedido);
+       System.out.println(os1);
+       System.out.println(os2);
     }
 }
