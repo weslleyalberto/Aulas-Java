97622ae85ab884f045c8667048e1f5f338a52ba9
Weslley P. Alberto <weslleyalberto@gmail.com>
2021-12-16 10:49:54 -0300

    entidade conta

---
+++ curso_avan_orien_Obj_Heranca/heranca_j/src/entities/Conta.java
@@ -0,0 +1,40 @@
+package entities;
+
+
+public class Conta {
+    private Integer numero;
+    private String holder;
+    protected Double saldo;
+    public Conta(Integer numero, String holder,Double saldo){
+        this.numero = numero;
+        this.holder = holder;
+        this.saldo = saldo;
+    }
+    public Conta(){
+
+    }
+    public Double getSaldo() {
+        return saldo;
+    }
+   
+    public String getHolder() {
+        return holder;
+    }
+    public void setHolder(String holder) {
+        this.holder = holder;
+    }
+    public Integer getNumero() {
+        return numero;
+    }
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+    public void saque(double saque){
+        saldo-= saque;
+    }
+    public void deposito(double deposito){
+        saldo+=deposito;
+    }
+  
+    
+}
