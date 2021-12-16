8a63f30c1df64ac2a51fcd3c9c31a92aa7305e19
Weslley P. Alberto <weslleyalberto@gmail.com>
2021-12-16 11:12:45 -0300

    Entidade usando a Conta como herença

---
+++ curso_avan_orien_Obj_Heranca/heranca_j/src/entities/ContaPoupanca.java
@@ -0,0 +1,22 @@
+package entities;
+
+public class ContaPoupanca extends Conta{
+    private Double taxaDeJuros;
+    public ContaPoupanca(){
+        super();
+    }
+    public Double getTaxaDeJuros() {
+        return taxaDeJuros;
+    }
+    public void setTaxaDeJuros(Double taxaDeJuros) {
+        this.taxaDeJuros = taxaDeJuros;
+    }
+    public ContaPoupanca(Integer numero,String holder,Double saldo, double taxaDeJuros){
+        super(numero,holder,saldo);
+        this.setTaxaDeJuros(taxaDeJuros);
+    }
+    public void atualizacaoSaldo(){
+        saldo += saldo * taxaDeJuros ;
+    }
+    
+}
