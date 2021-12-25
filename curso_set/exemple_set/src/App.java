9793b95e4a31ccd438f6b22552e66dbd6f7909f2
Weslley Alberto <weslleyalberto@gmail.com>
2021-12-25 15:07:51 -0300

    exemplo com a implementação tree

--- curso_set/exemple_set/src/App.java
+++ curso_set/exemple_set/src/App.java
@@ -1,8 +1,10 @@
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class App {
     public static void main(String[] args)  {
+        //Implementação HasSet
         Set<String> set = new HashSet<>();
         set.add("TV");
         set.add("Notebook");
@@ -11,6 +13,18 @@ public class App {
         System.out.println(set.contains("Notebook"));
         for(String s : set){
             System.out.println(s);
+        }
+        System.out.println("********************************************");
+        //Implementação TreeSet
+
+        Set<String> tree = new TreeSet<>();
+        tree.add("Marina");
+        tree.add("Joninha");
+        tree.add("Marinilza");
+
+        System.out.println(tree.contains("Marina"));
+        for(String c : tree){
+            System.out.println(c);
         }
     }
         
